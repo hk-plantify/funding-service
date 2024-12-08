117156f3026251f5c2fa3c931dd011c39a4d860c
zzoo0123 <0123suh@gmail.com>
2024-12-08 15:05:39 +0900

    chore: mongoDB -> MySQL

--- src/main/java/com/plantify/funding/FundingApplication.java
+++ src/main/java/com/plantify/funding/FundingApplication.java
@@ -2,10 +2,9 @@ package com.plantify.funding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableFeignClients
 public class FundingApplication {
 
